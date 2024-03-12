079c17e7f8a2eb0f4d66361df8da209951b6146e
Aayush Vashistha <aayush.vashistha@gmail.com>
2024-03-12 00:15:46 -0400

    added mongo connection

--- index.js
+++ index.js
@@ -3,32 +3,31 @@ const signin = require('./userController.js')
 const app = express()
 const PORT = 3000
 const path = require('path');
-
 const mongoose = require('mongoose');
-const { log } = require('console');
 
 app.use(express.json());
 
+
 async function middleware(req, res, next){
-    // console.log("This is after app.get ");
+    console.log("This is after signin", req.body);
     next();
 }
 
-app.get("/", middleware, (req, res) => {
+app.get("/", (req, res) => {
     // res.send("<h1> This is a homepage </h1>")
     res.sendFile(path.join(__dirname, 'home.html'))
     // console.log("**************************", req.headers);
 })
 
-// mongoose.connect("mongodb://127.0.0.1:27017/user")
-// .then(() => {
-//     app.listen(8000, () => {
-//         console.log("Mongo started on port 27017");
-//     });
-// })
-// .catch((error) => {
-//     console.log(error);
-// })
+mongoose.connect("mongodb://127.0.0.1:27017/user")
+.then(() => {
+    app.listen(8000, () => {
+        console.log("Mongo started on port 27017");
+    });
+})
+.catch((error) => {
+    console.log(error);
+})
 
 app.listen(PORT, (err) => {
     console.log(`server is listening at ${PORT}`);
@@ -41,7 +40,8 @@ app.get("/contact", (req, res) => {
     console.log("in contact");
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", middleware, (req, res) => {
     console.log("in sigin");
+    console.log(req.body);
     res.send("Form submitted")
 })
